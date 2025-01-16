4386b02c07bd174b43f31e6aa265c7c254a8a888
Tony Hirst <tony.hirst@gmail.com>
2025-01-16 22:44:53 +0000

    Update enrichers.py

--- src/dakar_rallydj/enrichers.py
+++ src/dakar_rallydj/enrichers.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
-def derive_clazz_metadata(withdrawn_teams_df, clazz_df, groups_df):
-    clazz_map_df = pd.merge(withdrawn_teams_df[["team.bib", "team.clazz"]], clazz_df[[
+def derive_clazz_metadata(x_df, clazz_df, groups_df, x_cols=None):
+    x_cols = ["team.bib", "team.clazz"] if x_cols is None or not (isinstance(x_cols, list) and len(x_cols)==2) else x_cols
+    clazz_map_df = pd.merge(x_df[x_cols], clazz_df[[
         "_id", "reference", "categoryClazz", "en"]], left_on="team.clazz", right_on="_id").drop(columns=["team.clazz", "_id"]).rename(columns={"en": "clazz_label"})
 
     clazz_map_df = pd.merge(clazz_map_df, groups_df[["reference", "tinyLabel", "label", "color", "en"]].rename(
         columns={"reference": "categoryClazz"}), on="categoryClazz").rename(columns={"en": "group_label"})
+    
+    # Reorder and reindex. Or should we leave it?
+    clazz_map_df.sort_values("team.bib", inplace=True)
+    clazz_map_df.reset_index(drop=True, inplace=True)
 
     return clazz_map_df
